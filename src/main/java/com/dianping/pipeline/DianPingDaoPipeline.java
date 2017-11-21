0197e1dad9fcfad9d8f50bc59e57cd48ef508eaf
zhangzhenyu <zhangzhenyu@wywk.cn>
2017-11-21 16:43:31 +0800

    update 加大频率

--- src/main/java/com/dianping/pipeline/DianPingDaoPipeline.java
+++ src/main/java/com/dianping/pipeline/DianPingDaoPipeline.java
@@ -1,5 +1,8 @@
 package com.dianping.pipeline;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.impl.Log4jLoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -17,11 +20,13 @@ import us.codecraft.webmagic.pipeline.PageModelPipeline;
 @Component("DianPingDaoPipeline")
 public class DianPingDaoPipeline implements PageModelPipeline<DianPingInfo> {
 
+	private  static Logger logger = LoggerFactory.getLogger(DianPingDaoPipeline.class);
     @Autowired
     private DianPingDAO jobInfoDAO;
 
     public void process(DianPingInfo dianPingInfo, Task task) {
     	System.out.println("正在添加"+dianPingInfo.getShopName());
+    	logger.info("正在添加"+dianPingInfo.getShopName());
     	jobInfoDAO.add(dianPingInfo);
     }
 }
